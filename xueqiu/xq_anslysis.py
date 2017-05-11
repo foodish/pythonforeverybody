7d36d3953b365efece69d27857876fd74ad61d81
foodish <xbj1900@gmail.com>
2017-05-11 16:50:18 +0800

    stable version1:单人关注列表抓取

--- xueqiu/xq_anslysis.py
+++ xueqiu/xq_anslysis.py
@@ -10,7 +10,13 @@ import networkx as nx
 
 def pagerank():
     conn = sqlite3.connect('xqfriends_0504.db')
-    following_data = pd.read_sql('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 5000) and to_id in (select id from People where fo_num > 5000)', conn)
+    # following_data = pd.read_sql('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 5000) and to_id in (select id from People where fo_num > 5000)', conn)
+    # following_data = pd.read_sql(
+    #     'select from_id, to_id from Follows where from_id in (select id from People where fo_num > 10000) and to_id '
+    #     'in (select id from People where fo_num > 10000)', conn)
+    following_data = pd.read_sql(
+        'select from_id, to_id from Follows where from_id in (select id from People where fo_num > 10000) and to_id '
+        'in (select id from People where fo_num > 10000)', conn)
     conn.close()
     
     G = nx.DiGraph()
@@ -25,6 +31,7 @@ def pagerank():
     pr_sorted = sorted(pr.items(), key=lambda x: x[1], reverse=True)
     print('pagerank top 100:\n')
     for p in pr_sorted[:100]:
+
         print(p[0], p[1])
         
 if __name__ == '__main__':
