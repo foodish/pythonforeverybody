43d36434cba26feaba364092f03ad970798e54f7
foodish <xbj1900@gmail.com>
2017-05-24 22:08:56 +0800

    add md file

--- xueqiu/xq_anslysis.py
+++ xueqiu/xq_anslysis.py
@@ -5,8 +5,9 @@
 
 import pandas as pd
 import sqlite3
-import numpy as np
+# import numpy as np
 import networkx as nx
+
 
 def mean_medium():
     conn = sqlite3.connect("xqfriends_0504.db")
@@ -22,7 +23,7 @@ def mean_medium():
 
 
 def density_centrality():
-    #密度中心性
+    # 密度中心性
     conn = sqlite3.connect("xqfriends_0504.db")
     following_data = pd.read_sql(
         'select from_id, to_id from Follows where from_id in (select id from People where fo_num > 50000) and to_id '
@@ -38,25 +39,24 @@ def density_centrality():
     for d in following_data.iterrows():
         G.add_edge(d[1][0], d[1][1])
 
-
     print('number of nodes:', G.number_of_nodes())
     print('number of edges:', G.number_of_edges())
 
-    ##print nx.average_shortest_path_length(G), '\n'
+    # print nx.average_shortest_path_length(G), '\n'
     print('density of graph:', nx.density(G))
 
     # user_betweenness_list = sorted(nx.betweenness_centrality(G).items(), lambda x, y: cmp(x[1], y[1]), reverse=True) #result like [(2, 0.0), (3, 0.0), (1, 1.0)]
-    # betweenness_list = zip(*user_betweenness_list)[1]# result like: [(2, 3, 1), (0.0, 0.0, 1.0)][1]; zip(*) is like un zip()
+    # betweenness_list = zip(*user_betweenness_list)[1]# result like: [(2, 3,
+    # 1), (0.0, 0.0, 1.0)][1]; zip(*) is like un zip()
 
-    #betweenness_count_pairs = collections.Counter(list(betweenness_list)).most_common() # list of element like: (0.0006937913420042883, 1)
-    #b_value = zip(*betweenness_count_pairs)[0] #unzip to get 0.0006937913420042883
+    # betweenness_count_pairs = collections.Counter(list(betweenness_list)).most_common() # list of element like: (0.0006937913420042883, 1)
+    # b_value = zip(*betweenness_count_pairs)[0] #unzip to get 0.0006937913420042883
     #b_count = zip(*betweenness_count_pairs)[1]
     #pylab.figure('Betweenness Distribution')
     #pylab.title('Betweenness Distribution')
-    #pylab.xlabel('Betweenness')
-    #pylab.ylabel('Count')
+    # pylab.xlabel('Betweenness')
+    # pylab.ylabel('Count')
     #pylab.scatter(b_value, b_count)
-
 
     # user_closeness_list = sorted(nx.closeness_centrality(G).items(), lambda x, y: cmp(x[1], y[1]), reverse=True) #Dict.items(): {1: 1.0, 2: 0.0, 3: 0.0} -> [(1, 1.0), (2, 0.0), (3, 0.0)]
     # closeness_list = zip(*user_closeness_list)[1]
@@ -66,8 +66,8 @@ def density_centrality():
     #c_count = zip(*closeness_count_pairs)[0]
     #pylab.figure('Closeness Distribution')
     #pylab.title('Closeness Distribution')
-    #pylab.xlabel('Closeness')
-    #pylab.ylabel('Count')
+    # pylab.xlabel('Closeness')
+    # pylab.ylabel('Count')
     #pylab.scatter(c_value, c_count)
 
 
@@ -81,27 +81,24 @@ def pagerank():
         'select from_id, to_id from Follows where from_id in (select id from People where fo_num > 10000) and to_id '
         'in (select id from People where fo_num > 10000)', conn)
     conn.close()
-    
+
     G = nx.DiGraph()
     cnt = 0
     for d in following_data.iterrows():
         G.add_edge(d[1][0], d[1][1])
         cnt += 1
     print('link numbers:', cnt)
-    
-    #PageRank
+
+    # PageRank
     pr = nx.pagerank(G)
     pr_sorted = sorted(pr.items(), key=lambda x: x[1], reverse=True)
     print('pagerank top 100:\n')
     for p in pr_sorted[:100]:
 
         print(p[0], p[1])
-        
+
 if __name__ == '__main__':
     # pagerank()
     # density_centrality()
     mean_medium()
 # In[ ]:
-
-
-
---
+++ xueqiu/xueqiu_analysis.md
@@ -0,0 +1,33 @@
+1. 总数据量
+    People:178591
+    Follows:2531570
+    People数据统计：
+                      fr_num        fo_num
+    count  178591.000000  1.785910e+05
+    mean       81.695567  7.391412e+02
+    std       181.335237  2.635567e+04
+    min         0.000000  0.000000e+00
+    25%        12.000000  2.000000e+00
+    50%        29.000000  8.000000e+00
+    75%        85.000000  4.000000e+0P1
+    max     25325.000000  5.084964e+06
+    fr_num    178591
+    fo_num    178591
+    dtype: int64
+    mean number of fr_num, fo_num: fr_num     81.695567
+    fo_num    739.141217
+    dtype: float64
+    medium number of fr_num, fo_num: fr_num    29.0
+    fo_num     8.0
+    dtype: float64
+2. 粉丝数分布
+    |>100000,| 62|
+    |>50000,| 220|
+    >10000, 2677
+    >5000, 4585
+    >1000,11424
+3. net50k:
+        |fr_num|fo_num|
+    |mean|437|215092|
+    |medium|150|74738|
+    
