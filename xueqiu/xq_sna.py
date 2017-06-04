a3502a925cfd94ebfdf2961315adc2785dcae8ae
foodish <xbj1900@gmail.com>
2017-06-04 15:54:25 +0800

    xueqiu sna

--- xueqiu/xq_sna.py
+++ xueqiu/xq_sna.py
@@ -22,80 +22,95 @@ def shortest():
     for i in p_all:
         print(i)
 
-def net5k():
-    cur.execute('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 5000) and to_id in (select id from People where fo_num > 5000)')
-    g = nx.DiGraph(name='net5k')
-    
-    for i in cur.fetchall():
-        g.add_edge(*i)
-        
-    print((nx.info(g)))
-        ##print nx.average_shortest_path_length(G), '\n'
-    print(('density of graph:', nx.density(g)))
        
-def density_centrality():
-    cur.execute('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 100000) and to_id in (select id from People where fo_num > 100000)')
-    g = nx.DiGraph(name='net5k')
+def centrality():
+    #cur.execute('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 50000) and to_id in (select id from People where fo_num > 50000)')
+    cur.execute('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 100000)')
+    
+    g = nx.DiGraph(name='net100k')
     
     for i in cur.fetchall():
         g.add_edge(*i)
-        
+    
+    print(g.name)
+    
+    print('节点数为:\n', g.number_of_nodes())
+    print('关系数为:\n', g.number_of_edges())  
+    
     in_degree = g.in_degree()    
     sorted_in_degree = sorted(in_degree.items(), key=lambda item:item[1], reverse=True)
-    print('top 10 in_degree of graph:\n',sorted_in_degree[0:10])
     
+    print('\ntop 10 in_degree of graph:\n')    
+    for i in range(10):
+        id, fans_num = sorted_in_degree[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
+        
     closeness = nx.closeness_centrality(g)        
     sorted_closeness = sorted(closeness.items(), key=lambda item:item[1], reverse=True)
-    print('top 10 closeness_centrality of graph:\n',sorted_closeness[0:10])
+    
+    print('\ntop 10 closeness_centrality of graph:\n')    
+    for i in range(10):
+        id, fans_num = sorted_closeness[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
     
     betweenness = nx.betweenness_centrality(g)
     sorted_betweeness = sorted(closeness.items(), key=lambda item:item[1], reverse=True)
-    print('top 10 betweenness_centrality of graph:\n',sorted_betweeness[0:10])
+    
+    print('\ntop 10 betweenness_centrality of graph:\n')    
+    for i in range(10):
+        id, fans_num = sorted_betweeness[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
     
     eigenvector = nx.eigenvector_centrality(g)
     sorted_eigenvector = sorted(eigenvector.items(), key=lambda item:item[1], reverse=True)
-    print('top 10 eigenvector_centrality of graph:\n',sorted_betweeness[0:10])
     
-    ##print nx.average_shortest_path_length(G), '\n'
-    #print('density of graph:', nx.density(g))
-    ##user_betweenness_list = sorted(list(betweenness.items()), lambda x, y: cmp(x[1], y[1]), reverse=True) #result like [(2, 0.0), (3, 0.0), (1, 1.0)]
-    ##betweenness_list = zip(*user_betweenness_list)[1]# result like: [(2, 3, 1), (0.0, 0.0, 1.0)][1]; zip(*) is like un zip()
-
-    #betweenness_count_pairs = collections.Counter(list(betweenness_list)).most_common() # list of element like: (0.0006937913420042883, 1)
-    #b_value = zip(*betweenness_count_pairs)[0] #unzip to get 0.0006937913420042883
-    #b_count = zip(*betweenness_count_pairs)[1]    
-
-
-    ##user_closeness_list = sorted(list(closeness.items()), lambda x, y: cmp(x[1], y[1]), reverse=True) #Dict.items(): {1: 1.0, 2: 0.0, 3: 0.0} -> [(1, 1.0), (2, 0.0), (3, 0.0)]
-    ##closeness_list = zip(*user_closeness_list)[1]   
-
-    #closeness_count_pairs = collections.Counter(list(closeness_list)).most_common()
-    #c_value = zip(*closeness_count_pairs)[0]
-    #c_count = zip(*closeness_count_pairs)[0]
-
+    print('\ntop 10 eigenvector_centrality of graph:\n')   
+    for i in range(10):
+        id, fans_num = sorted_eigenvector[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
+        
     
-def pagerank_hits():
-    cur.execute('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 5000) and to_id in (select id from People where fo_num > 5000)')
-    g = nx.DiGraph(name='net5k')
-    
-    for i in cur.fetchall():
-        g.add_edge(*i)
-
     # PageRank
     pr = nx.pagerank(g)
-    prsorted = sorted(list(pr.items()), key=lambda x: x[1], reverse=True)
-    print('pagerank top 100:\n')
-    for p in prsorted[:100]:
-        print(p[0], p[1])
+    prsorted = sorted(pr.items(), key=lambda item:item[1], reverse=True)
+    #prsorted = sorted(list(pr.items()), key=lambda x: x[1], reverse=True)
+    
+    print('\npagerank top 10:\n')    
+    for i in range(10):
+        id, fans_num = prsorted[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
+
     
     # HITS
     hub, auth = nx.hits(g)
-    print('hub top 100:\n')
-    for h in sorted(list(hub.items()), key=lambda x: x[1], reverse=True)[:100]:
-        print(h[0], h[1])
-    print('\nauth top 100:\n')    
-    for a in sorted(list(auth.items()), key=lambda x: x[1], reverse=True)[:100]:     
-        print(a[0], a[1])
+    hub_sorted = sorted(hub.items(), key=lambda item:item[1], reverse=True)
+    auth_sorted = sorted(auth.items(), key=lambda item:item[1], reverse=True)
+    
+    print('\nhub top 10:\n')
+    for i in range(10):
+        id, fans_num = hub_sorted[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
+
+        
+    print('\nauth top 10:\n')
+    for i in range(10):
+        id, fans_num = auth_sorted[i]
+        cur.execute('select name, fo_num from People where id = ?', (id,))
+        name, fo_num = cur.fetchone()
+        print(name, fo_num, fans_num)
+
 
 def strongly_connected_components():
     cur.execute('select from_id, to_id from Follows where from_id in (select id from People where fo_num > 5000) and to_id in (select id from People where fo_num > 5000)')
@@ -125,10 +140,8 @@ def strongly_connected_components():
 if __name__ == '__main__':
     print('---------starting----------')
     #shortest()
-    #net5k()
     #strongly_connected_components()
-    #pagerank_hits()
-    density_centrality()
+    centrality()
     print('---------ending---------')
     '''
     Name: net5k
